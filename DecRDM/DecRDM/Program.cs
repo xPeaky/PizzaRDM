3f15d60ec15d50c0898990844ba8b2adc7d22fbe
Trytax <baiku1@hotmail.fr>
2017-06-20 10:49:52 +0200

    Ajout de DecRDM et décodage automatique de RDM_Decrypt

---
+++ DecRDM/DecRDM/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DecRDM
+{
+	class MainClass
+	{
+		/* Original code
+		 var dec:Function = function(param1:String, param2:String):String
+            {
+               var _loc3_:ByteArray = HumanCheck["decodeToByteArray"](param1);
+               var _loc4_:ByteArray = HumanCheck["decodeToByteArray"](param2);
+               var _loc5_:uint = 0;
+               while(_loc5_ < _loc3_.length)
+               {
+                  _loc3_[_loc5_] = _loc3_[_loc5_] ^ _loc4_[_loc5_ % _loc4_["length"]];
+                  _loc5_++;
+               }
+               return _loc3_.toString();
+            };
+            */
+		public static void Main(string[] args)
+		{
+			Console.Write("Chemin du fichier HumanCheck.as: ");
+			string filePath = Console.ReadLine();
+			if (!System.IO.File.Exists(filePath))
+			{
+				Console.WriteLine("Ce fichier n'existe pas !");
+			}
+			else
+			{
+				string content = System.IO.File.ReadAllText(filePath);
+				string pattern = @"dec\(\""(.+?)\"",\""(.+?)\""\)";
+				string result = Regex.Replace(content, pattern, m =>
+				 {
+					 string encrypted = m.Groups[1].Value;
+					 string key = m.Groups[2].Value;
+					return "\"" + dec(encrypted,key) + "\"";
+				 });
+
+				Console.Write("Chemin du fichier décodé: ");
+				filePath = Console.ReadLine();
+				System.IO.File.WriteAllText(filePath, result);
+			}
+
+			/*while (true)
+			{
+				Console.Write(">>> ");
+				string command = Console.ReadLine();
+				if (command == "exit")
+				{
+					break;
+				}
+				string[] splitted = command.Split(' ');
+				Console.WriteLine(dec(splitted[0], splitted[1]));
+			}*/
+		}
+
+		public static string dec(string param1, string param2)
+		{
+			byte[] _loc3_ = Convert.FromBase64String(param1);
+			byte[] _loc4_ = Convert.FromBase64String(param2);
+			uint _loc5_ = 0;
+
+			while (_loc5_ < _loc3_.Length)
+			{
+				_loc3_[_loc5_] = (byte)(_loc3_[_loc5_] ^ _loc4_[_loc5_ % _loc4_.Length]);
+				_loc5_++;
+			}
+
+			return Encoding.ASCII.GetString(_loc3_);
+		}
+	}
+}
--- RDM_Decrypt/RDM_Decrypt/Program.cs
+++ RDM_Decrypt/RDM_Decrypt/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace RDM_Decrypt
 {
@@ -13,7 +14,28 @@ namespace RDM_Decrypt
 			binaryDatas.Add(File.ReadAllBytes("5__a_-_.bin"));
 			binaryDatas.Add(File.ReadAllBytes("6__a_---.bin"));
 
-			while (true)
+			Console.Write("Chemin du fichier HumanCheck.as: ");
+			string filePath = Console.ReadLine();
+			if (!System.IO.File.Exists(filePath))
+			{
+				Console.WriteLine("Ce fichier n'existe pas !");
+			}
+			else
+			{
+				string content = System.IO.File.ReadAllText(filePath);
+				string pattern = @"§_a_-_---§\.§_a_--_--§\(-([0-9]*)\)";
+				string result = Regex.Replace(content, pattern, m =>
+				 {
+					 int value = -(Convert.ToInt32(m.Groups[1].Value));
+					return "\"" + First.GetDefinitionName(value, binaryDatas) + "\"";
+				 });
+
+				Console.Write("Chemin du fichier décodé: ");
+				filePath = Console.ReadLine();
+				System.IO.File.WriteAllText(filePath, result);
+			}
+
+			/*while (true)
 			{
 				Console.Write(">>> ");
 				string command = Console.ReadLine();
@@ -23,7 +45,7 @@ namespace RDM_Decrypt
 				}
 
 				Console.WriteLine(First.GetDefinitionName(Convert.ToInt32(command), binaryDatas));
-			}
+			}*/
 		}
 	}
 }
